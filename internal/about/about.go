71b84dbaa5b7ed50891e5571767d1c719f4285e1
Akatsuki <masahisakun@gmail.com>
2026-06-20 10:08:29 +0900

    chore: prepare v1.2.3-dev2

--- internal/about/about.go
+++ internal/about/about.go
@@ -2,8 +2,8 @@ package about
 
 const (
 	AppName        = "ImagePadServer"
-	Version        = "v1.2.2-dev5"
-	FileVersion    = "1.2.2.5"
+	Version        = "v1.2.3-dev2"
+	FileVersion    = "1.2.3.2"
 	Author         = "Akat / 赤月さん"
 	License        = "MIT License"
 	Copyright      = "Copyright (c) 2026 Akat / 赤月さん"
